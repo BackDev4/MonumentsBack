5f20b6d2b20b86460939f1fa515f04e88215e8d4
iliao <iliaosipov.134@gmail.com>
2024-02-12 15:00:38 +0300

    fix

--- startup.sh
+++ startup.sh
@@ -4,8 +4,7 @@ sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
 php-fpm -D
 
+php artisan key:generate
 # while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
 
 nginx
-
-php artisan key:generate
