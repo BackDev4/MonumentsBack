4b656d5be3c3d75e1dfbb6eea0c4ca1a50245066
iliao <iliaosipov.134@gmail.com>
2024-02-12 15:09:56 +0300

    Fix

--- startup.sh
+++ startup.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
+# Замена порта в конфигурации Nginx
 sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 
+# Генерация файла .env
+cp /app/.env.example /app/.env
+
+# Генерация ключа приложения Laravel
+php artisan key:generate
+
+# Настройка подключения к базе данных
+sed -i "s/DB_CONNECTION=mysql/DB_CONNECTION=pgsql/" /app/.env
+sed -i "s/DB_HOST=127.0.0.1/DB_HOST=104.198.53.159/" /app/.env
+sed -i "s/DB_PORT=3306/DB_PORT=5432/" /app/.env
+sed -i "s/DB_DATABASE=your_database/DB_DATABASE=monuments/" /app/.env
+sed -i "s/DB_USERNAME=your_username/DB_USERNAME=postgres/" /app/.env
+sed -i "s/DB_PASSWORD=your_password/DB_PASSWORD=20050404iliA/" /app/.env
+
+# Установка правильных разрешений для каталога storage
+chmod -R 777 /app/storage
+
+# Запуск PHP-FPM
 php-fpm -D
 
-php artisan key:generate
-chmod -R 777 /app/storage
-# while ! nc -w 1 -z 127.0.0.1 9000; do sleep 0.1; done;
-
+# Запуск Nginx
 nginx
