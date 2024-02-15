43873d3ef91d3c2d95f1c16d72d3383a6984d5d2
iliao <iliaosipov.134@gmail.com>
2024-02-15 09:38:47 +0300

    fix

--- startup.sh
+++ startup.sh
@@ -6,14 +6,15 @@ sed -i "s,LISTEN_PORT,$PORT,g" /etc/nginx/nginx.conf
 # Генерация файла .env
 cp /app/.env.example /app/.env
 
-./cloud_sql_proxy -dir=/cloudsql &
+# Запуск Cloud SQL Proxy
+/cloud_sql_proxy -dir=/cloudsql -instances=steel-sonar-413417:us-central1:data-base-monument &
 
 # Генерация ключа приложения Laravel
 php artisan key:generate
 
 # Настройка подключения к базе данных
 sed -i "s/DB_CONNECTION=mysql/DB_CONNECTION=pgsql/" /app/.env
-sed -i "s/DB_HOST=127.0.0.1/DB_HOST=/cloudsql/steel-sonar-413417:us-central1:data-base-monument/" /app/.env
+sed -i "s/DB_HOST=127.0.0.1/DB_HOST=unix:/cloudsql/steel-sonar-413417:us-central1:data-base-monument/" /app/.env
 sed -i "s/DB_PORT=3306/DB_PORT=5432/" /app/.env
 sed -i "s/DB_DATABASE=your_database/DB_DATABASE=monuments/" /app/.env
 sed -i "s/DB_USERNAME=your_username/DB_USERNAME=postgres/" /app/.env
