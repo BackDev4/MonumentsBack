3ab2b692c2d98b973f4c138d11e83d7967b1f490
iliao <iliaosipov.134@gmail.com>
2024-02-15 09:49:58 +0300

    fix

--- startup.sh
+++ startup.sh
@@ -13,12 +13,12 @@ cp /app/.env.example /app/.env
 php artisan key:generate
 
 # Настройка подключения к базе данных
-sed -i "s/DB_CONNECTION=mysql/DB_CONNECTION=pgsql/" /app/.env
-sed -i "s/DB_HOST=127.0.0.1/DB_HOST=unix:/cloudsql/steel-sonar-413417:us-central1:data-base-monument/" /app/.env
-sed -i "s/DB_PORT=3306/DB_PORT=5432/" /app/.env
-sed -i "s/DB_DATABASE=your_database/DB_DATABASE=monuments/" /app/.env
-sed -i "s/DB_USERNAME=your_username/DB_USERNAME=postgres/" /app/.env
-sed -i "s/DB_PASSWORD=your_password/DB_PASSWORD=20050404iliA/" /app/.env
+sed -i "s,DB_CONNECTION=mysql,DB_CONNECTION=pgsql," /app/.env
+sed -i "s,DB_HOST=127.0.0.1,DB_HOST=unix:/cloudsql/steel-sonar-413417:us-central1:data-base-monument," /app/.env
+sed -i "s,DB_PORT=3306,DB_PORT=5432," /app/.env
+sed -i "s,DB_DATABASE=your_database,DB_DATABASE=monuments," /app/.env
+sed -i "s,DB_USERNAME=your_username,DB_USERNAME=postgres," /app/.env
+sed -i "s,DB_PASSWORD=your_password,DB_PASSWORD=20050404iliA," /app/.env
 
 php artisan migrate
 php artisan adminlte:install
