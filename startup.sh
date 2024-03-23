b292dcfd2ec719721346ac27406699901688024b
BackDev4 <iliaosipov.134@gmail.com>
2024-03-23 10:29:56 +0300

    fix

--- startup.sh
+++ startup.sh
@@ -8,6 +8,11 @@ apk add --no-cache postgresql
 /etc/init.d/postgresql setup
 /etc/init.d/postgresql start
 
+# Изменение конфигурации PostgreSQL для принятия внешних подключений
+su postgres -c "sed -i 's/#listen_addresses = 'localhost'/listen_addresses = '*'/g' /etc/postgresql/postgresql.conf"
+echo 'host    all             all             0.0.0.0/0               md5' | su postgres -c "tee -a /var/lib/postgresql/data/pg_hba.conf"
+su postgres -c "/etc/init.d/postgresql reload"
+
 # Создание базы данных и пользователя PostgreSQL
 su postgres -c 'createdb monuments'
 su postgres -c "psql -c \"CREATE USER postgres WITH PASSWORD '20050404iliA';\""
@@ -18,7 +23,7 @@ php artisan key:generate
 
 # Настройка подключения к базе данных PostgreSQL
 echo "DB_CONNECTION=pgsql" >> /app/.env
-echo "DB_HOST=localhost" >> /app/.env
+echo "DB_HOST=127.0.0.1" >> /app/.env
 echo "DB_PORT=5432" >> /app/.env
 echo "DB_DATABASE=monuments" >> /app/.env
 echo "DB_USERNAME=postgres" >> /app/.env
@@ -27,7 +32,7 @@ echo "DB_PASSWORD=20050404iliA" >> /app/.env
 cat /app/.env
 
 # Проверка статуса PostgreSQL и выполнение миграций и других операций Laravel
-pg_ctl status | grep "no server running" && exit 1
+su postgres -c "pg_ctl status | grep 'no server running' && exit 1"
 composer update
 php artisan migrate
 php artisan storage:link
